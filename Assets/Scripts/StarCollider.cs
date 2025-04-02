8520a483266f5692c93ec59d74a94354ea5424f4
Loyan06 <laiacampos625@gmail.com>
2025-04-02 15:44:09 +0200

    coin not working

--- Assets/Scripts/StarCollider.cs
+++ Assets/Scripts/StarCollider.cs
@@ -16,7 +16,7 @@ public class DestroyOnTrigger : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         Destroy(gameObject);
     }
